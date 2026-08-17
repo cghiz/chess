4f25b14bb72c06f00f9e3c806233117fded6434b
cghiz <cghiz@student.bridgew.edu>
2026-08-16 22:49:59 -0400

    pawn.java

--- pawn.java
+++ pawn.java
@@ -16,16 +16,20 @@ public class pawn extends Piece
     public boolean canMove(int startx, int starty, int endx, int endy)
     {
         if (color == true) {
+            //moves forward
             if (endx == startx && endy == starty - 1) {
+                //only by one
                 if (Chess.position[endx][endy] == null) {
                     return true;
                 }
             }
+            //IF FIRST MOVE CAN GO 2 FOR EACH PAWN
             if (starty == 6 && endx == startx && endy == starty - 2) {
                 if (Chess.position[startx][starty - 1] == null && Chess.position[endx][endy] == null) {
                     return true;
                 }
             }
+            //capture diagonally
             if (Math.abs(endx - startx) == 1 && endy == starty - 1) {
                 if (Chess.position[endx][endy] != null && Chess.position[endx][endy].color != color) {
                     return true;
@@ -34,16 +38,19 @@ public class pawn extends Piece
         }
         else
         {
+            //black pawn gets its own
             if (endx == startx && endy == starty + 1) {
                 if (Chess.position[endx][endy] == null) {
                     return true;
                 }
             }
+            //black pawn FIRST MOVE of pawn can also go 2
             if (starty == 1 && endx == startx && endy == starty + 2) {
                 if (Chess.position[startx][starty + 1] == null && Chess.position[endx][endy] == null) {
                     return true;
                 }
             }
+            //captures diagnally also 
             if (Math.abs(endx - startx) == 1 && endy == starty + 1)
             {
                 if (Chess.position[endx][endy] != null && Chess.position[endx][endy].color != color)
