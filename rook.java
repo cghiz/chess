e39618065e7adad88255736b205275a579ae78f9
cghiz <cghiz@student.bridgew.edu>
2026-08-16 22:15:41 -0400

    Create rook.java

---
+++ rook.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+public class rook extends Piece
+{
+    public rook(boolean color)
+    {
+        super(color);
+        if (color==true)
+        {
+            image= new ImageIcon("src/wrook.png");
+        }
+        else
+        {
+            image= new ImageIcon("src/brook.png");
+        }
+    }
+    public boolean canMove(int startx, int starty, int endx,int endy)
+    {
+        if (startx!=endx&&starty!=endy) {
+            return false;
+        }
+        int xDirection=0;
+        int yDirection=0;
+        if (endx>startx) {
+            xDirection = 1;
+        }
+        else if  (endx<startx)
+        {
+            xDirection = -1;
+        }
+        else if(endy>starty)
+        {
+            yDirection = 1;
+        }
+        else if (endy<starty)
+        {
+            yDirection = -1;
+        }
+        int x= startx+xDirection;
+        int y= starty+yDirection;
+        while(x!=endx ||y!=endy) {
+            if (Chess.position[x][y] != null) {
+                return false;
+            }
+            x += xDirection;
+            y += yDirection;
+        }
+        if (Chess.position[endx][endy]==null)
+        {
+            return true;
+        }
+        if (Chess.position[endx][endy].color!= color)
+        {
+            return true;
+        }
+        return false;
+    }
+}
